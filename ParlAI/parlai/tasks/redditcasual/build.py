f316a3e515b4008e213e9568c2753b9400e3ca19
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-09-15 19:59:46 -0400

    save dir redditcasual

--- ParlAI/parlai/tasks/redditcasual/build.py
+++ ParlAI/parlai/tasks/redditcasual/build.py
@@ -46,7 +46,6 @@ def build(opt):
             save_dir = project_dir.joinpath('ParlAI', 'data', 'redditcasual')
             if not os.path.isdir(save_dir):
                 os.mkdir(save_dir)
-                
             save_path = save_dir.joinpath(f'{split}.txt')
             with open(save_path, 'w', encoding='utf-8') as f:
                 for conv in data:
