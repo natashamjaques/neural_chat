5bc1f211abcb584d94d9bbb392401ee610147a40
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-09-15 23:39:48 -0400

    debugging

--- HierarchicalRL/hrl_tune.py
+++ HierarchicalRL/hrl_tune.py
@@ -190,10 +190,10 @@ class REINFORCETuner:
 
             # Index corresponding to the start of each conversation
             start_idx = np.cumsum([0] + conversation_length.tolist()[:-1])
-            responses = [output[0][s+l-1].tolist()
+            responses = [output[0][s+l-1, ].tolist()
                          for s, l in zip(start_idx, conversation_length.tolist())]
             decoded = [self.policy_net.vocab.decode(resp) for resp in responses]
-            print([len(decoded[i]) for i, c in enumerate(conversations)])
+            print([len(c) for i, c in enumerate(conversations)])
             _ = [conv.append(decoded[i]) for i, conv in enumerate(conversations)]
 
             if not simulated_turn:
