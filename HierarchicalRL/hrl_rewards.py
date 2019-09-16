30a1acfe6e752af5027cda459738b08ffcbeb826
AbdulSaleh <35547218+AbdulSaleh@users.noreply.github.com>
2019-09-15 23:22:49 -0400

    debugging

--- HierarchicalRL/hrl_rewards.py
+++ HierarchicalRL/hrl_rewards.py
@@ -239,8 +239,8 @@ def reward_bot_deepmoji(conversations):
     episode_len = (len(conversations[0]) - 1) // 2
     # Flattened bot responses
     bot_responses = [resp for conv in conversations for resp in conv[1::2]]
-    print(len(bot_responses))
-    
+    print(num_convs, episode_len, len(bot_responses))
+
     # Run deepmoji
     reward_multiplier = _get_reward_multiplier()
     bot_emojis = botmoji.encode_multiple(bot_responses)
